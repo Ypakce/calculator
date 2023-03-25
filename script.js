e580e29e545623db464bcbc6bd605b6a3902ca36
Ypakce <yagizpakce@gmail.com>
2023-03-25 23:26:35 +0300

    make calculator work

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let num1;
-let num2;
-let operator;
+let num1 = undefined;
+let num2 = undefined;
+let operator = undefined;
+let previousAns = "";
+
+const numButtons = Array.from(document.querySelectorAll(".num"));
+const currentResult = document.querySelector(".currentResult");
+const previousResult = document.querySelector(".previousResult");
+const clearButton = document.querySelector("#clear");
+const backButton = document.querySelector("#back");
+const opeButtons = Array.from(document.querySelectorAll(".ope"));
+const equalButton = document.querySelector(".equal");
 
 function add(num1, num2){
     return num1 + num2;
@@ -15,6 +24,10 @@ function multiply(num1, num2){
 }
 
 function divide(num1, num2){
+    if(num2 == 0){
+        alert("Can't divide by zero");
+        return;
+    }
     return num1 / num2;
 }
 
@@ -35,4 +48,52 @@ function operate(operator, num1, num2){
     return res;
 }
 
-console.log(operate("*", 10, 25));
+clearButton.addEventListener("click", () => {
+    currentResult.textContent = "";
+    previousResult.textContent = "";
+    num1 = undefined;
+    num2 = undefined;
+    operator = undefined;
+})
+
+backButton.addEventListener("click", () => {
+    currentResult.textContent = previousAns;
+})
+
+numButtons.forEach(numButton => {
+    numButton.addEventListener("click", () => {
+        previousAns = currentResult.textContent.toString();
+        let num = numButton.textContent.toString();
+        if(num === "." && previousAns.includes(".")){
+            return;
+        }
+        else{
+            let currentAns = previousAns + num;
+            currentResult.textContent = currentAns;
+        }
+    })
+})
+
+opeButtons.forEach(opeButton => {
+    opeButton.addEventListener("click", () => {
+        num1 = currentResult.textContent;
+        operator = opeButton.textContent;
+        previousResult.textContent = num1 + " " + operator;
+        currentResult.textContent = "";
+    })
+})
+
+equalButton.addEventListener("click", () => {
+    if(num1 == undefined || operator == undefined){
+        return;
+    }
+    else{
+        num2 = currentResult.textContent;
+        let result;
+        result = operate(operator, parseFloat(num1), parseFloat(num2));
+        previousResult.textContent = result.toString();
+        currentResult.textContent = "";
+        num2 = undefined;
+        operator = undefined;
+    }
+})
